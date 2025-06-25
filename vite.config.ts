8ab89b98fb927a23f99d8149e6e6e3a18c42437a
aspxa <163057757+AlexMorariu86@users.noreply.github.com>
2025-06-26 01:16:25 +0300

    Configure GitHub Pages Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
-export default defineConfig(({ mode }) => ({
-  // Deploy to subdirectory of your custom domain
-  base: mode === "production" ? "/portofolio-xp/" : "/",
-  
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [
-    react(),
-  ],
-
+export default defineConfig({
+  plugins: [react()],
+  base: '/portofolio-xp/', // Replace with your actual repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-}));
+})
