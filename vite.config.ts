ec03937afcd124b3c5dc60831c8f4725a4c094ec
aspxa <163057757+AlexMorariu86@users.noreply.github.com>
2025-06-26 02:19:00 +0300

    al 100-lea commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio-xp', // Set the base path for the project
-  build: {
-    sourcemap: false, // Disable source maps in production
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  base: '/portofolio-xp/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
